b9122050def41e71ff7f9d670099cab7dd1bc182
Leandro Rodrigues <78665194+LeandroRodrigues061@users.noreply.github.com>
2024-09-25 11:10:48 -0300

    Commit 25/09 Leandro
    Feito Footer e melhoria na Section 2.

--- components/SectionServices.tsx
+++ components/SectionServices.tsx
@@ -12,17 +12,17 @@ export default function Services(){
     };
 
     return(
-    <div className="w-full flex flex-row justify-around sm: flex-col items-center md:flex-row">
+    <div className="w-full flex flex-row justify-around gap-5 sm: flex-col items-center md:flex-row">
 
         <div className="flex flex-col p-5 pt-10 w-[450px]">
             <div className="h-[100px] w-[500px]">
                 <div className="flex flex-row">
-                     <h1 className="flex flex-row font-rubik font-bold text-black text-4xl">Simplicidade e práticidade</h1><span className="flex flex-row bg-rosinha w-3 h-3 rounded-full p-1 relative top-6 mx-1"></span>
+                     <h1 className="font-rubik font-bold text-black text-4xl md: text-3xl ">Simplicidade e práticidade</h1><span className="flex flex-row bg-rosinha w-2 h-2 rounded-full p-1 relative top-6 mx-1"></span>
                 </div>
-                <h6 className="text-xs">Mostrar as funcionalidades da nossa aplicação bem aqui</h6>
+                <h6 className="font-rubik text-xs">Mostrar as funcionalidades da nossa aplicação bem aqui</h6>
             </div>
 
-            <div className="h-[664px] gap-7  flex flex-col text-lg">
+            <div className="h-full gap-5 flex flex-col text-lg">
 
                 <ServiceCard1
                     isSelected={selectedCard ===1}
@@ -34,6 +34,8 @@ export default function Services(){
                     asperiores odio delectus voluptatum."
                 />
 
+                    <hr />
+
                 <ServiceCard2
                     isSelected={selectedCard ===2}
                     onSelect={() => handleSelecteCard(2)}
@@ -43,6 +45,8 @@ export default function Services(){
                     Quaerat sit earum magnam cum ipsam voluptate distinctio 
                     asperiores odio delectus voluptatum."
                 />
+
+                    <hr />
 
                 <ServiceCard3
                     isSelected={selectedCard ===3}
@@ -58,14 +62,14 @@ export default function Services(){
         </div>
 
 
-        <div className="flex flex-col justify-center w-full sm: max-w-[30em] md:max-w-[55em] lg:max-w-[55em]">
+        <div className="flex flex-col justify-center w-full sm: max-w-[35em] md:max-w-[55em] lg:max-w-[55em]">
 
             <div className="flex justify-center items-center bg-laranjinha h-[500px] rounded-lg">
                 <p>Inserir a tela deseja ao selecionar aqui</p>
             </div>
 
-            <div className="ml-10 mt-10 flex justify-start">
-                <button className="bg-gradient-to-r from-RosinhaEscurinho to-laranjinha text-white border w-[15em] h-[3em] rounded-xl flex flex-row text-xl items-center text-center justify-center"> 
+            <div className="ml-10 mt-10 flex sm:justify-center m-5">
+                <button className="bg-gradient-to-r from-RosinhaEscurinho to-laranjinha text-white border w-[17em] h-[3em] rounded-2xl flex flex-row text-xl items-center text-center justify-center "> 
                     {iconeCalendario} Planejar agora
                 </button>   
             </div>
---
+++ components/icons/facebook.tsx
@@ -0,0 +1,13 @@
+export const FacebookLogo =(
+
+<svg width="26" height="27" viewBox="0 0 26 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0_122_38)">
+<path d="M7.44958 11.2382V15.4953H10.6424V22.9453H14.8995V15.4953H18.0923L19.1566 11.2382H14.8995V9.1097C14.8995 8.82744 15.0116 8.55673 15.2112 8.35714C15.4108 8.15755 15.6815 8.04542 15.9638 8.04542H19.1566V3.78833H15.9638C14.5525 3.78833 13.1989 4.34897 12.201 5.34692C11.203 6.34487 10.6424 7.69838 10.6424 9.1097V11.2382H7.44958Z" stroke="#A9A9A9" stroke-width="2.12855" stroke-linecap="round" stroke-linejoin="round"/>
+</g>
+<defs>
+<clipPath id="clip0_122_38">
+<rect width="25.5426" height="25.5426" fill="white" transform="translate(0 0.595459)"/>
+</clipPath>
+</defs>
+</svg>
+)
