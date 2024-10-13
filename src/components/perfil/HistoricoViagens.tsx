7dcff2f69001ad467fbe1e90d80854a6c54ecd09
RAUL DOS SANTOS ARAUJO <122099251+R4ULz@users.noreply.github.com>
2024-10-13 17:05:04 -0300

    Commit 13/10 - Pagina de perfil completa

--- src/components/perfil/HistoricoViagens.tsx
+++ src/components/perfil/HistoricoViagens.tsx
@@ -4,8 +4,8 @@ export default function HistoricoViagens() {
     return (
         <div className="bg-white w-full h-full rounded-xl border-[1px] shadow-xl border-zinc-400 flex flex-col">
             <div className="h-full py-14 max-hd:py-5 max-hd:px-5 px-20 flex justify-center items-center flex-col space-y-10">
-                <Image src={"/imgs/manutencao.png"} alt="manutencao" width={180} height={180} className="size-40"></Image>
-                <h1 className="text-4xl text-zinc-700">Aqui teremos a página de histórico de viagens</h1>
+                <Image src={"/imgs/manutencao.png"} alt="manutencao" width={180} height={180} className="size-40 max-hd:size-20"></Image>
+                <h1 className="text-4xl text-zinc-700 max-hd:text-2xl">Aqui teremos a página de histórico de viagens</h1>
             </div>
         </div>
     )
--- src/components/perfil/LoginESenha.tsx
+++ src/components/perfil/LoginESenha.tsx
@@ -4,8 +4,8 @@ export default function LoginESenha() {
     return (
         <div className="bg-white w-full h-full rounded-xl border-[1px] shadow-xl border-zinc-400 flex flex-col">
             <div className="h-full py-14 max-hd:py-5 max-hd:px-5 px-20 flex justify-center items-center flex-col space-y-10">
-                <Image src={"/imgs/manutencao.png"} alt="manutencao" width={180} height={180} className="size-40"></Image>
-                <h1 className="text-4xl text-zinc-700">Aqui teremos a página de Login e Senha</h1>
+                <Image src={"/imgs/manutencao.png"} alt="manutencao" width={180} height={180} className="size-40 max-hd:size-20"></Image>
+                <h1 className="text-4xl max-hd:text-2xl text-zinc-700">Aqui teremos a página de Login e Senha</h1>
             </div>
         </div>
     )
